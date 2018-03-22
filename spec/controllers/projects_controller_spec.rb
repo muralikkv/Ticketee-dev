2d71114aa9824f8e4df70187c466b9ba861b0ff0
muralikkv <murali_1401@yahoo.co.in>
2018-03-22 17:12:45 +0530

    Redirect the users back to the projects page if they

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
-describe ProjectsController do
+describe ProjectsController, type: :controller do
+
+    it "handles a missing project correctly" do
+        get :show, id: "not-here"
+      
+        response.should redirect_to(projects_path)
+      
+        message = "The project you were looking for could not be found."
+        flash[:alert].should eq message
+    end
 
 end
